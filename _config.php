26f061b992b827b6d9dcdff357c5c524faaa3653
Bram de Leeuw <bram-de-leeuw@hotmail.com>
2020-06-06 11:42:14 +0200

    register for google sitemap

--- _config.php
+++ _config.php
@@ -1,1 +1,7 @@
 <?php
+
+$googleSiteMap = 'Wilr\GoogleSitemaps\GoogleSitemap';
+if (class_exists($googleSiteMap)) {
+    $googleSiteMap::register_dataobject(XD\Shopify\Model\Product::class);
+    $googleSiteMap::register_dataobject(XD\Shopify\Model\Collection::class);
+}
