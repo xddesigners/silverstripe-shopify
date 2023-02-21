5248b5b224d6927963819ee4e695de42128686b5
Bram de Leeuw <bram-de-leeuw@hotmail.com>
2023-02-21 18:02:39 +0100

    Check if ShopifyPage exists before registering the objects to google sitemap

--- _config.php
+++ _config.php
@@ -1,7 +1,7 @@
 <?php
 
 $googleSiteMap = 'Wilr\GoogleSitemaps\GoogleSitemap';
-if (class_exists($googleSiteMap)) {
+if (class_exists($googleSiteMap) && DataObject::get_one(ShopifyPage::class)) {
     $googleSiteMap::register_dataobject(XD\Shopify\Model\Product::class);
     $googleSiteMap::register_dataobject(XD\Shopify\Model\Collection::class);
 }
