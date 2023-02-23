1a78b46bd09f667ef504203e1a80255540685702
Bram de Leeuw <bram-de-leeuw@hotmail.com>
2023-02-23 11:30:27 +0100

    update includes

--- _config.php
+++ _config.php
@@ -1,9 +1,12 @@
 <?php
 
 use SilverStripe\ORM\DataObject;
+use XD\Shopify\Model\Collection;
+use XD\Shopify\Model\Product;
+use XD\Shopify\Model\ShopifyPage;
 
 $googleSiteMap = 'Wilr\GoogleSitemaps\GoogleSitemap';
 if (class_exists($googleSiteMap) && DataObject::get_one(ShopifyPage::class)) {
-    $googleSiteMap::register_dataobject(XD\Shopify\Model\Product::class);
-    $googleSiteMap::register_dataobject(XD\Shopify\Model\Collection::class);
+    $googleSiteMap::register_dataobject(Product::class);
+    $googleSiteMap::register_dataobject(Collection::class);
 }
